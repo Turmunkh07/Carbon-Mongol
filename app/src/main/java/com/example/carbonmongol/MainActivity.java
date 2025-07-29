9f5b10b083e137d724871f9a0f5607f3d261a341
turmu <turmunkh.0702@gmail.com>
2025-07-29 10:17:15 +0800

    Day1-Morning: Firebase Configuration + Register/Login pages

--- app/src/main/java/com/example/carbonmongol/MainActivity.java
+++ app/src/main/java/com/example/carbonmongol/MainActivity.java
@@ -1,27 +1,27 @@
 package com.example.carbonmongol;
+import com.google.firebase.FirebaseApp;
 
+import android.content.Intent;
 import android.os.Bundle;
-
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-
 import com.example.carbonmongol.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ActivityMainBinding binding;
+private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        FirebaseApp.initializeApp(this);
+     binding = ActivityMainBinding.inflate(getLayoutInflater());
+     setContentView(binding.getRoot());
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
@@ -32,6 +32,13 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
+        goToPage(Register.class);
     }
 
+    public void goToPage(Class cal){
+        Intent intent = new Intent(getApplicationContext(), cal);
+        startActivity(intent);
+        finish();
+    }
 }
